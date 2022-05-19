bfbaf5f5a62839290a6e6d9a7fb374f86afbaa5c
pthtran22 <92424532+pthtran22@users.noreply.github.com>
2022-05-19 12:45:22 -0700

    Add files via upload

---
+++ Scanner.rs
@@ -0,0 +1,29 @@
+// mod Token;
+// mod Cstream;
+
+// use crate::Cstream::CStream;
+
+// struct Scanner {
+//     all_tokens: Vec<Token>,
+// }
+
+// impl Scanner {
+//     fn new () -> Scanner {
+            //all_tokens = // empty vector
+//     }
+
+//     fn get_next_token() -> Token {
+            
+//     }
+// }
+
+// // using content from Cstream use get_next_char to get the char and call get_next_token to return Token Type and save it into a vector and return
+// fn get_all_tokens () -> Vec<TokenTypes> { 
+
+// }
+
+
+// pub fn run() {
+//     let all_tokens: Vec<TokenType>;  // initilize empty vector
+//     let mut ex = Scanner::new(Cstream.get_content());
+// }
