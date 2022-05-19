8ab112aeae3bc46f223918fc449d9ed5815193b1
pthtran22 <pthtran@ucdavis.edu>
2022-05-19 16:10:37 -0700

    changed mod files

--- main.rs
+++ main.rs
@@ -1,8 +1,17 @@
-mod Cstream;
+// use crate::Cstream::CStream;
+// use crate::Token::Token;
+// use crate::Scanner::Scanner;
+
 mod Token;
+mod Cstream;
 mod Scanner;
+// use Scanner::new;
+// pub use new
 
 fn main (){
-    Token::run();
+    //let file = CStream::run();
+    let file = Cstream::run();
+    Scanner::run(&file);
+
 }
 
