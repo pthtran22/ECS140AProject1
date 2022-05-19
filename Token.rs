6e545a9d51568d6e776ca3400b31164dffd6e712
pthtran22 <pthtran@ucdavis.edu>
2022-05-19 16:12:09 -0700

    started check_TokenType

--- Token.rs
+++ Token.rs
@@ -9,7 +9,9 @@ enum TokenType {
     Invalid,
 }
 
-struct Token {
+let keywords: vec!["unsigned", "char", "short", "int", "long", "float", "double", "while", "if", "return", "void", "main"]
+ 
+pub struct Token {
     text: String,
     token_type: TokenType,
     line_num: i32,
@@ -29,7 +31,8 @@ impl Token {
     }
 
     fn get_next_char(&mut self) -> Option<char> {
-		let cur = CStream::get_content().chars().nth((self.overall_pos) as usize);          // get_content() from Cstream will give me my file
+        
+		let cur = self.text.chars().nth((self.overall_pos) as usize);          // get_content() from Cstream will give me my file
 		match cur {
 			None => {       // NULL ??
 				self.char_pos += 1;
@@ -48,11 +51,64 @@ impl Token {
 			}
 		}
 		self.overall_pos += 1;
-		CStream::get_content().chars().nth((self.overall_pos) as usize)  // this returns my character
+		self.text.chars().nth((self.overall_pos) as usize)  // this returns my character
 	}
+
+    fn check_TokenType(mut self, text) -> TokenType {
+        // Keyword
+        if keyword.contains(text) {
+            return TokenType::Keyword;
+        }
+
+        // Identifier
+        if text.chars().nth(0).unwrap() == '_' || isalpha(text.chars().nth(0).unwrap()) {
+            if text.to_string.len() == 1 {
+                return TokenType::Identifier;
+            }
+            for c in 1..text.to_string().len() {
+                if text.chars().nth(c).unwrap() != '_' && !isalphanumeric(text.chars().nth(0).unwrap()) {
+                    return TokenType::Invalid;
+                }
+            }
+            return TokenType::Identifier;
+        }
+
+
+        // IntConstant
+        let dotCount = 0;
+        // checks if the first two characters are (-) digit
+        if text.chars().nth(0).unwrap() == '-' || isnumeric(text.chars().nth(0).unwrap()) {
+            if text.to_string.len() == 1 { // only digit
+                if isnumeric(text.chars().nth(0).unwrap()) {
+                    return TokenType::IntConstant;
+                }
+                return TokenType::Invalid;
+            }
+            // checks if the rest of the characters are digit.{digit}
+            for c in 1..text.to_string().len() {
+                if !isnumeric(text.chars().nth(c).unwrap()) {
+                    if text.chars().nth(c).unwrap() == '.' && c !=  text.to_string.len() - 1{
+                        dotCount += 1;
+                    }
+                    return TokenType::Invalid;
+                }
+            }
+            if dotCount == 0 {
+                return TokenType::IntConstant;
+            }
+            if dotCount == 1 {
+                return TokenType::Float;
+            }
+            else {
+                return TokenType::Invalid;
+            }
+        }
+        }
+    }
 }
 
 pub fn run() {
-    println!("hi");
+    // Token::new("hi", TokenType::Keyword);
+    // println!("hi");
     // Cstream::run();
 }
