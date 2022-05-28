6c6d2676a5923fa198efec01463c40107b1d2e83
Annemarie Xia <63884529+xxinjing-1020@users.noreply.github.com>
2022-05-28 16:05:36 -0700

    Update main.rs

--- project/main.rs
+++ project/main.rs
@@ -12,15 +12,17 @@ fn main (){
     let file = Cstream::run();
     let mut ex = Scanner::Scanner::new(file.to_string());
     let all_tokens = ex.get_all_tokens();
-    for i in 0..all_tokens.len() {
-        println!("--------------------------");
-        println!("token: {:?}", all_tokens[i].text);
-        println!("token_type: {:?}", all_tokens[i].token_type);
-        println!("line_num: {:?}", all_tokens[i].line_num);
-        println!("char_pos: {:?}", all_tokens[i].char_pos);
+    let mut parser = Parser::Parser::new(all_tokens);
+    let boolean = parser.Program();
+    // for i in 0..all_tokens.len() {
+    //     println!("--------------------------");
+    //     println!("token: {:?}", all_tokens[i].text);
+    //     println!("token_type: {:?}", all_tokens[i].token_type);
+    //     println!("line_num: {:?}", all_tokens[i].line_num);
+    //     println!("char_pos: {:?}", all_tokens[i].char_pos);
         
-    }
-    println!("--------------------------");
+    // }
+    // println!("--------------------------");
     
 
 }
