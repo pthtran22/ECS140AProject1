86c35b01829b613bf727c1eb40dcdab51219c285
Annemarie Xia <63884529+xxinjing-1020@users.noreply.github.com>
2022-05-29 23:52:00 -0700

    Update main.rs

--- project/main.rs
+++ project/main.rs
@@ -6,16 +6,19 @@ mod Token;
 mod Cstream;
 mod Scanner;
 mod Parser;
-//use Scanner::Scanner;
-// pub use new
+
+// custom_error!{MyError
+//     General{n: bool} = "Function return {n}.",
+// }
+
 
 fn main (){
     let file = Cstream::run();
     let mut ex = Scanner::Scanner::new(file.to_string());
     let all_tokens = ex.get_all_tokens();
-        
-    // check if there's ==, >=, <=, !=
+
     for i in 0..all_tokens.len()-1 {
+        // check ==, >=, <=, !=
         if i > 0 && all_tokens[i].text == "=" {
             if all_tokens[i-1].text == "=" {
                 all_tokens.remove(i);
@@ -35,10 +38,18 @@ fn main (){
             }
         }
     }
-    
-    
+
     let mut parser = Parser::Parser::new((&all_tokens).to_vec());
-    let boolean = parser.Program();
+    let result = parser.Program();
+    // if result == true {
+    //     println!("Input program is syntactacilly correct.");
+    // }
+    // match result
+    // {
+    //     Ok(n) => println!("Input program is syntactacilly correct."),
+    //     Err(n) => eprintln!("Syntax error at character position"),
+    // }
+
     // for i in 0..all_tokens.len() {
     //     println!("--------------------------");
     //     println!("token: {:?}", all_tokens[i].text);
@@ -52,3 +63,12 @@ fn main (){
 
 }
 
+
+
+
+
+// match result
+// {
+//     Ok(n) => println!("Input program is syntactacilly correct."),
+//     Err(n) => eprintln!("Error at Line {} Character {}. The syntax should be: {}.", line_num, char_pos, grammar_rule),
+// }
